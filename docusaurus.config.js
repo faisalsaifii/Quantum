54a483fe4fc687c74a0784eed92006a6012413e5
faisalsaifii <m.faisal.s@outlook.com>
2024-01-24 17:51:05 +0530

    refactor: categorised into semesters

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -2,122 +2,197 @@ import { themes as prismThemes } from 'prism-react-renderer';
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'Quantum',
-  favicon: 'img/favicon.ico',
-  url: 'https://quantumdocs.vercel.app',
-  baseUrl: '/',
-  organizationName: 'faisalsaifii', // Usually your GitHub org/user name.
-  projectName: 'Quantum', // Usually your repo name.
+	title: 'Quantum',
+	favicon: 'img/favicon.ico',
+	url: 'https://quantumdocs.vercel.app',
+	baseUrl: '/',
+	organizationName: 'faisalsaifii', // Usually your GitHub org/user name.
+	projectName: 'Quantum', // Usually your repo name.
 
-  onBrokenLinks: 'throw',
-  onBrokenMarkdownLinks: 'warn',
+	onBrokenLinks: 'throw',
+	onBrokenMarkdownLinks: 'warn',
 
-  i18n: {
-    defaultLocale: 'en',
-    locales: ['en'],
-  },
-  markdown: {
-    mermaid: true,
-  },
-  themes: [
-    '@docusaurus/theme-mermaid',
-    // '@docusaurus/theme-search-algolia'
-  ],
-  plugins: [require.resolve('docusaurus-lunr-search')],
+	i18n: {
+		defaultLocale: 'en',
+		locales: ['en'],
+	},
+	markdown: {
+		mermaid: true,
+	},
+	themes: [
+		'@docusaurus/theme-mermaid',
+		// '@docusaurus/theme-search-algolia'
+	],
+	plugins: [require.resolve('docusaurus-lunr-search')],
 
-  presets: [
-    [
-      'classic',
-      /** @type {import('@docusaurus/preset-classic').Options} */
-      ({
-        docs: {
-          sidebarPath: './sidebars.js',
-          editUrl:
-            'https://github.com/faisalsaifii/Quantum/tree/main',
-        },
-        theme: {
-          customCss: './src/css/custom.css',
-        },
-      }),
-    ],
-  ],
+	presets: [
+		[
+			'classic',
+			/** @type {import('@docusaurus/preset-classic').Options} */
+			({
+				docs: {
+					sidebarPath: './sidebars.js',
+					editUrl:
+						'https://github.com/faisalsaifii/Quantum/tree/main',
+				},
+				theme: {
+					customCss: './src/css/custom.css',
+				},
+			}),
+		],
+	],
 
-  themeConfig:
-    /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
-    ({
-      // Replace with your project's social card
-      tableOfContents: {
-        minHeadingLevel: 2,
-        maxHeadingLevel: 6,
-      },
-      image: 'img/social-card.jpg',
-      navbar: {
-        title: 'Quantum',
-        logo: {
-          alt: 'Logo',
-          src: 'img/logo.svg',
-        },
-        items: [
-          {
-            type: 'docSidebar',
-            sidebarId: 'tutorialSidebar',
-            position: 'left',
-            label: 'Artificial Intelligence',
-          },
-          // {
-          //   type: 'docSidebar',
-          //   sidebarId: 'tutorialSidebar',
-          //   position: 'left',
-          //   label: 'Cloud Computing',
-          // },
-          {
-            href: 'https://github.com/faisalsaifii/Quantum',
-            label: 'GitHub',
-            position: 'right',
-          },
-        ],
-      },
-      footer: {
-        style: 'dark',
-        links: [
-          {
-            title: 'Docs',
-            items: [
-              {
-                label: 'Artificial Intelligence',
-                to: '/docs/category/artificial-intelligence',
-              },
-              // {
-              //   label: 'Cloud Computing',
-              //   to: '/docs/Cloud Computing/about',
-              // },
-            ],
-          },
-          {
-            title: 'Community',
-            items: [
-              {
-                label: 'Stack Overflow',
-                href: 'https://stackoverflow.com/users/14247140/faisal',
-              },
-              {
-                label: 'Linkedin',
-                href: 'https://www.linkedin.com/in/faisalsaifii',
-              },
-              {
-                label: 'Twitter',
-                href: 'https://twitter.com/faisalsaifiii',
-              },
-            ],
-          },
-        ],
-        copyright: `Copyright © ${new Date().getFullYear()}`,
-      },
-      prism: {
-        theme: prismThemes.github,
-        darkTheme: prismThemes.dracula,
-      },
-    }),
+	themeConfig:
+		/** @type {import('@docusaurus/preset-classic').ThemeConfig} */
+		({
+			// Replace with your project's social card
+			tableOfContents: {
+				minHeadingLevel: 2,
+				maxHeadingLevel: 6,
+			},
+			image: 'img/social-card.jpg',
+			navbar: {
+				title: 'Quantum',
+				logo: {
+					alt: 'Logo',
+					src: 'img/logo.svg',
+				},
+				items: [
+					{
+						type: 'docSidebar',
+						sidebarId: 'allSemsSidebar',
+						position: 'left',
+						label: 'All Semesters',
+					},
+					{
+						type: 'docSidebar',
+						sidebarId: 'sem1Sidebar',
+						position: 'left',
+						label: 'Sem 1',
+					},
+					{
+						type: 'docSidebar',
+						sidebarId: 'sem2Sidebar',
+						position: 'left',
+						label: 'Sem 2',
+					},
+					{
+						type: 'docSidebar',
+						sidebarId: 'sem3Sidebar',
+						position: 'left',
+						label: 'Sem 3',
+					},
+					{
+						type: 'docSidebar',
+						sidebarId: 'sem4Sidebar',
+						position: 'left',
+						label: 'Sem 4',
+					},
+					{
+						type: 'docSidebar',
+						sidebarId: 'sem5Sidebar',
+						position: 'left',
+						label: 'Sem 5',
+					},
+					{
+						type: 'docSidebar',
+						sidebarId: 'sem6Sidebar',
+						position: 'left',
+						label: 'Sem 6',
+					},
+					{
+						type: 'docSidebar',
+						sidebarId: 'sem7Sidebar',
+						position: 'left',
+						label: 'Sem 7',
+					},
+					{
+						type: 'docSidebar',
+						sidebarId: 'sem8Sidebar',
+						position: 'left',
+						label: 'Sem 8',
+					},
+					{
+						href: 'https://github.com/faisalsaifii/Quantum',
+						label: 'GitHub',
+						position: 'right',
+					},
+				],
+			},
+			footer: {
+				style: 'dark',
+				links: [
+					{
+						title: 'Semesters',
+						items: [
+							{
+								label: '1st Semester',
+								to: '/docs/category/1st-semester',
+							},
+							{
+								label: '2nd Semester',
+								to: '/docs/category/2nd-semester',
+							},
+							{
+								label: '3rd Semester',
+								to: '/docs/category/3rd-semester',
+							},
+							{
+								label: '4th Semester',
+								to: '/docs/category/4th-semester',
+							},
+							{
+								label: '5th Semester',
+								to: '/docs/category/5th-semester',
+							},
+							{
+								label: '6th Semester',
+								to: '/docs/category/6th-semester',
+							},
+							{
+								label: '8th Semester',
+								to: '/docs/category/8th-semester',
+							},
+						],
+					},
+					{
+						title: 'Subjects',
+						items: [
+							{
+								label: 'Artificial Intelligence',
+								to: '/docs/category/artificial-intelligence',
+							},
+							{
+								label: 'Renewable Energy Resources',
+								to: '/docs/category/renewable-energy-resources',
+							},
+						],
+					},
+					{
+						title: 'Socials',
+						items: [
+							{
+								label: 'Stack Overflow',
+								href: 'https://stackoverflow.com/users/14247140/faisal',
+							},
+							{
+								label: 'Linkedin',
+								href: 'https://www.linkedin.com/in/faisalsaifii',
+							},
+							{
+								label: 'Twitter',
+								href: 'https://twitter.com/faisalsaifiii',
+							},
+						],
+					},
+				],
+				copyright: `Copyright © ${new Date().getFullYear()}`,
+			},
+			prism: {
+				theme: prismThemes.github,
+				darkTheme: prismThemes.dracula,
+			},
+		}),
 };
 
 export default config;
--- src/pages/index.js
+++ src/pages/index.js
@@ -8,36 +8,38 @@ import Heading from '@theme/Heading';
 import styles from './index.module.css';
 
 function HomepageHeader() {
-  const { siteConfig } = useDocusaurusContext();
-  return (
-    <header className={clsx('hero hero--primary', styles.heroBanner)}>
-      <div className="container">
-        <Heading as="h1" className="hero__title">
-          {siteConfig.title}
-        </Heading>
-        <p className="hero__subtitle">{siteConfig.tagline}</p>
-        <div className={styles.buttons}>
-          <Link
-            className="button button--secondary button--lg"
-            to="/docs/category/artificial-intelligence">
-            Get started
-          </Link>
-        </div>
-      </div>
-    </header>
-  );
+	const { siteConfig } = useDocusaurusContext();
+	return (
+		<header className={clsx('hero hero--primary', styles.heroBanner)}>
+			<div className='container'>
+				<Heading as='h1' className='hero__title'>
+					{siteConfig.title}
+				</Heading>
+				<p className='hero__subtitle'>{siteConfig.tagline}</p>
+				<div className={styles.buttons}>
+					<Link
+						className='button button--secondary button--lg'
+						to='/docs/category/1st-semester'
+					>
+						Get started
+					</Link>
+				</div>
+			</div>
+		</header>
+	);
 }
 
 export default function Home() {
-  const { siteConfig } = useDocusaurusContext();
-  return (
-    <Layout
-      title={`${siteConfig.title}`}
-      description="One place for all the docs you need">
-      <HomepageHeader />
-      <main>
-        <HomepageFeatures />
-      </main>
-    </Layout>
-  );
+	const { siteConfig } = useDocusaurusContext();
+	return (
+		<Layout
+			title={`${siteConfig.title}`}
+			description='One place for all the docs you need'
+		>
+			<HomepageHeader />
+			<main>
+				<HomepageFeatures />
+			</main>
+		</Layout>
+	);
 }
